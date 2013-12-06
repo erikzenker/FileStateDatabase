1212fe9aab5be3477c716212dcd4a3e4acf1be14
Erik Zenker <erikzenker@hotmail.com>
2013-12-06 15:06:15 +0100

    * Better performance with FileStateCache, new method getFileState

--- main.cc
+++ main.cc
@@ -1,5 +1,9 @@
 #include <FileStateDatabase.h>
+#include <FileState.h>
 #include <iostream>
+#include <map>
+#include <string>
+#include <boost/filesystem.hpp>
 
 int main(int argc, char **argv){
 
@@ -11,17 +15,22 @@ int main(int argc, char **argv){
   boost::filesystem::path dir(argv[1]);
   FileStateDatabase fileStateDatabase("my_filestate_database", dir);
 
-  // Get updates since last getUpdates
-  std::vector<std::pair<FileState, ModState> > updates = fileStateDatabase.getUpdates();
+  //Get updates since last getUpdates
+  std::vector<std::pair<FileState, ModState> > updates = fileStateDatabase.updates();
 
   for(auto update = updates.begin(); update != updates.end(); ++update){
     FileState fs = update->first;
     ModState ms  = update->second;
-    std::cout << "Update: " << fs.path << " " << fs.modtime << " " << FileStateDatabase::modStateToString(ms) << std::endl;
+    std::cout << "Update: " << fs.path.string() << " " << fs.modtime << " " << FileStateDatabase::modStateToString(ms) << std::endl;
 
     // Propagate modstate into database
     fileStateDatabase.propagateUpdate(*update);
   }
 
+   FileState fs_tmp = fileStateDatabase.getFileState("/home/erik/test/adhoc.sh");
+   std::cout << "printFileState: \"" << fs_tmp.path.string() << "\" " << fs_tmp.modtime <<  std::endl;
+
   return 0;
+
+  
 }
